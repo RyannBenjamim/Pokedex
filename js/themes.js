4c6d2b9f95c296b4c1aff8cad4af5d79749dec05
Ryan Benjamim <87036259+RyannBenjamim@users.noreply.github.com>
2023-01-27 11:01:26 -0300

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -9,6 +9,8 @@ themes.addEventListener('click', function() {
         root.style.setProperty('--bg-color', '#f2f2f2')
         root.style.setProperty('--bg-color2', '#fff')
         root.style.setProperty('--bg-color3', '#f2f2f2')
+        root.style.setProperty('--modal-color', '#f2f2f2')
+        root.style.setProperty('--modal-color2', '#fff')
         root.style.setProperty('--btn-text', '#fff')
         root.style.setProperty('--text-primary', '#000')
         root.style.setProperty('--text-secundary', '#black')
@@ -19,6 +21,8 @@ themes.addEventListener('click', function() {
         root.style.setProperty('--bg-color', 'linear-gradient(90deg, rgba(23,50,89,1) 6%, rgba(16,38,69,1) 26%, rgba(14,33,60,1) 90%)')
         root.style.setProperty('--bg-color2', '#1d3457')
         root.style.setProperty('--bg-color3', '#172A46')
+        root.style.setProperty('--modal-color', '#1d3457')
+        root.style.setProperty('--modal-color2', '#172A46')
         root.style.setProperty('--text-primary', '#fdfdfd')
         root.style.setProperty('--text-secundary', '#ebe7e7')
         themes.dataset.theme ='dark'
